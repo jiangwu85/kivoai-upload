4a1783f7fe793e921e3bff8a9a4eece6d8378999
jw <273617974@qq.com>
2025-12-15 23:59:13 +0800

    init 03

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 
-
-    async function authorize(request, env) {
-      const authorization = request.headers.get("authorization");
-      const access_token = authorization ? authorization.replace("Bearer ", "") : null;
-      if (!access_token) {
-        return new Response("Unauthorized", { status: 401 });
-      }
-      const user_data = await env.REDIS.get("auth_" + access_token);
-      if (!user_data) {
-        return new Response("Unauthorized", { status: 401 });
-      }
-      return user_data;
-    }
+//
+//    async function authorize(request, env) {
+//      const authorization = request.headers.get("authorization");
+//      const access_token = authorization ? authorization.replace("Bearer ", "") : null;
+//      if (!access_token) {
+//        return new Response("Unauthorized", { status: 401 });
+//      }
+//      const user_data = await env.REDIS.get("auth_" + access_token);
+//      if (!user_data) {
+//        return new Response("Unauthorized", { status: 401 });
+//      }
+//      return user_data;
+//    }
 
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const path = url.pathname;
     if (path === "/list") {
-        user_data = authorize(request, env);
-        if (user_data instanceof Response) {
-            return user_data;
-        }
+//        user_data = authorize(request, env);
+//        if (user_data instanceof Response) {
+//            return user_data;
+//        }
       const objects = await env.MY_BUCKET.list();
       return Response.json(
             {
@@ -35,10 +35,10 @@ export default {
             });
     }
     if (path.startsWith("/upload/")) {
-        user_data = authorize(request, env);
-        if (user_data instanceof Response) {
-            return user_data;
-        }
+//        user_data = authorize(request, env);
+//        if (user_data instanceof Response) {
+//            return user_data;
+//        }
       authorize(request, env);
       const key = path.slice("/upload/".length);
       if (!key) {
@@ -53,10 +53,10 @@ export default {
             });
     }
     if (path.startsWith("/download/")) {
-        user_data = authorize(request, env);
-        if (user_data instanceof Response) {
-            return user_data;
-        }
+//        user_data = authorize(request, env);
+//        if (user_data instanceof Response) {
+//            return user_data;
+//        }
       authorize(request, env);
       const key = path.slice("/download/".length);
       if (!key) {
@@ -73,10 +73,10 @@ export default {
       });
     }
     if (path.startsWith("/delete/")) {
-        user_data = authorize(request, env);
-        if (user_data instanceof Response) {
-            return user_data;
-        }
+//        user_data = authorize(request, env);
+//        if (user_data instanceof Response) {
+//            return user_data;
+//        }
       authorize(request, env);
       const key = path.slice("/delete/".length);
       if (!key) {
