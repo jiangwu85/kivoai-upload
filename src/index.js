b257203848b736619a7e81e0f7e70c9da14b4d01
jw <273617974@qq.com>
2025-12-15 23:22:48 +0800

    init 04

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ const corsHeaders = {
 
 export default {
   async fetch(request, env) {
+     const authorization = request.headers.get("authorization");
+     const access_token = authorization ? authorization.replace("Bearer ", "") : null;
+     if (!access_token) {
+       return new Response("Unauthorized", { status: 401 });
+     }
+     const user_data = await env.MY_KV.get("auth_" + access_token);
+     if (!user_data) {
+       return new Response("Unauthorized", { status: 401 });
+     }
+
+
     const url = new URL(request.url);
     const path = url.pathname;
 
